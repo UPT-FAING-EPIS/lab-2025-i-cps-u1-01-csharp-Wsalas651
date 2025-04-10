5057d2b9f6af7d3107b7789edb93f7b8feaab592
Wsalas651 <ws2022073896@virtual.upt.pe>
2025-04-09 23:58:43 -0500

    Correccion

--- Bank/Bank.Domain/Models/BankAccount.cs
+++ Bank/Bank.Domain/Models/BankAccount.cs
@@ -2,26 +2,36 @@ namespace Bank.Domain.Models
 {
     public class BankAccount
     {
-        private readonly string m_customerName = string.Empty;
+        private readonly string m_customerName;
         private double m_balance;
-        private BankAccount() { }
+
+        private BankAccount()
+        {
+            m_customerName = string.Empty;
+            m_balance = 0;
+        }
+
         public BankAccount(string customerName, double balance)
         {
-            m_customerName = customerName;
+            m_customerName = customerName ?? throw new ArgumentNullException(nameof(customerName), "Customer name cannot be null.");
             m_balance = balance;
         }
+
         public string CustomerName { get { return m_customerName; } }
-        public double Balance { get { return m_balance; }  }
+        public double Balance { get { return m_balance; } }
         public void Debit(double amount)
         {
-            ArgumentOutOfRangeException.ThrowIfGreaterThan(amount, m_balance, nameof(amount));
-            ArgumentOutOfRangeException.ThrowIfNegative(amount, nameof(amount));
+            if (amount > m_balance)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
             m_balance -= amount;
         }
 
         public void Credit(double amount)
         {
-            ArgumentOutOfRangeException.ThrowIfNegative(amount, nameof(amount));
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
             m_balance += amount;
         }
     }
