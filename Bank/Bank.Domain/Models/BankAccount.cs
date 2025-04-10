e61c4628206b4fc5fe8fd1df15e65cc78d28904c
Wsalas651 <ws2022073896@virtual.upt.pe>
2025-04-10 18:58:46 -0500

    Sonar

--- Bank/Bank.Domain/Models/BankAccount.cs
+++ Bank/Bank.Domain/Models/BankAccount.cs
@@ -2,36 +2,26 @@ namespace Bank.Domain.Models
 {
     public class BankAccount
     {
-        private readonly string m_customerName;
+        private readonly string m_customerName = string.Empty;
         private double m_balance;
-
-        private BankAccount()
-        {
-            m_customerName = string.Empty;
-            m_balance = 0;
-        }
-
+        private BankAccount() { }
         public BankAccount(string customerName, double balance)
         {
-            m_customerName = customerName ?? throw new ArgumentNullException(nameof(customerName), "Customer name cannot be null.");
+            m_customerName = customerName;
             m_balance = balance;
         }
-
         public string CustomerName { get { return m_customerName; } }
-        public double Balance { get { return m_balance; } }
+        public double Balance { get { return m_balance; }  }
         public void Debit(double amount)
         {
-            if (amount > m_balance)
-                throw new ArgumentOutOfRangeException(nameof(amount));
-            if (amount <= 0)
-                throw new ArgumentOutOfRangeException(nameof(amount));
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(amount, m_balance, nameof(amount));
+            ArgumentOutOfRangeException.ThrowIfNegative(amount, nameof(amount));
             m_balance -= amount;
         }
 
         public void Credit(double amount)
         {
-            if (amount <= 0)
-                throw new ArgumentOutOfRangeException(nameof(amount));
+            ArgumentOutOfRangeException.ThrowIfNegative(amount, nameof(amount));
             m_balance += amount;
         }
     }
